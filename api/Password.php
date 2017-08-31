2b84981b9b261bfd1f0dfa4dd555fae9bbcbde54
kevin <kevin@loren>
2017-08-30 20:31:58 -0600

    API Change Password

--- api/Password.php
+++ api/Password.php
@@ -8,7 +8,6 @@ $passNew = $_POST['passUpdate'];
 
 if (isset($user) && isset($passOld) && isset($passNew) && $user != "" && $passOld != "" && $passOld != "" && isset($id)) {
     $userBusiness = new UserBusiness();
-    $userBusiness = new UserBusiness();
     $person = $userBusiness->isStudent($_POST['username'], $_POST['userpassword']);
     if ($person != NULL) {
         if ($userBusiness->updatePassword($person->getPersonId(), $passOld, $passNew) == 1) {
