430ce3aa802deff81abf6960a4748702f7b1254f
KevinEsquivel21 <kevinesquivel21@gmail.com>
2018-09-24 21:48:31 -0600

    Circular

--- actions/CircularDeleteAction.php
+++ actions/CircularDeleteAction.php
@@ -6,7 +6,11 @@ $id = $_GET['id'];
 
 if (isset($id)) {
     $business = new CircularBusiness();
+    
+    $circular = $business->get($id);
+
     if ($business->delete($id)) {
+        unlink("../../documents/circular/".$circular->getCircularGUID().".pdf");
         header("location: ../view/CircularShow.php?action=1&msg=Registro_eliminado_correctamente");
     } else {
         header("location: ../view/CircularShow.php?action=0&msg=Error_al_eliminar_registro");
